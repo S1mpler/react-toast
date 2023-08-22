9cf3b00b458ddf8a4f2b322b568be2a15c9c9317
S1mpler <stas.dior@gmail.com>
2023-08-22 09:46:06 +0200

    Add tailwind and icon lib to the project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        info: '#2196f3',
+        danger: '#f44336',
+        warning: '#ff9800',
+      }
+    },
+  },
+  plugins: [],
+}
+
